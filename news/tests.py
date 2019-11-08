712adc2743fc18fbd767405ef3a5e78eb0272a4b
Isaacg94 <7248zack@gmail.com>
2019-11-08 12:55:50 +0300

    initial commit

---
+++ news/tests.py
@@ -0,0 +1,64 @@
+from django.test import TestCase
+from .models import Editor,Article,tags
+import datetime as dt
+
+# Create your tests here.
+class EditorTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.isaac= Editor(first_name = 'Isaac', last_name ='Gichuru', email ='7248zack@gmail.com')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.isaac,Editor))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.isaac.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
+
+# class TagsTestClass(TestCase):
+
+#     # Set up method
+#     def setUp(self):
+#         self.tech= tags(name = 'Tech')
+
+#     # Testing  instance
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.tech,Editor))
+
+class ArticleTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+        self.james.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.james)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
+
+    def test_get_news_today(self):
+        today_news = Article.todays_news()
+        self.assertTrue(len(today_news)>0)
+
+    def test_get_news_by_date(self):
+        test_date = '2017-03-17'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        news_by_date = Article.days_news(date)
+        self.assertTrue(len(news_by_date) == 0)
+
+    
+
